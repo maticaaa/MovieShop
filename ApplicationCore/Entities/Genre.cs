e98e1a4af786ef3f6029d7f270b27a14692e42fa
maticaaa <43811770+maticaaa@users.noreply.github.com>
2021-11-06 17:57:00 -0400

    Create Genre.cs

---
+++ ApplicationCore/Entities/Genre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    [Table("Genre")]
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [MaxLength(24)]
+        public string Name { get; set; }
+
+        // Navigation Property
+        public ICollection<MovieGenre> Movies { get; set; }
+    }
+}
